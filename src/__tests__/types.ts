01c57c9dfbac3d1759555cdfcc039c7d16dab055
Syrus Akbary <me@syrusakbary.com>
2018-02-12 23:58:56 -0800

    Added query testing

--- src/__tests__/types.ts
+++ src/__tests__/types.ts
@@ -2,7 +2,7 @@ import * as Sequelize from "sequelize";
 import { Model } from "./../utils";
 import { SequelizeObjectType } from "./../types";
 import { Registry } from "./../registry";
-import { getGraphQLType } from "graphene-js";
+import { getGraphQLType, ObjectType, Field, ID, Schema } from "graphene-js";
 import { GraphQLObjectType } from "../../../graphene-js/node_modules/@types/graphql";
 
 describe("utils", () => {
@@ -17,12 +17,13 @@ describe("utils", () => {
     lastName: { type: Sequelize.STRING, comment: "The last name" }
   });
 
-  beforeEach(() => {
-    model.sync();
+  beforeEach(async () => {
+    await sequelize.sync();
+    await model.sync();
   });
 
-  afterEach(() => {
-    model.drop();
+  afterEach(async () => {
+    await model.drop();
   });
 
   test("SequelizeObjectType works", () => {
@@ -46,5 +47,54 @@ describe("utils", () => {
       @SequelizeObjectType({ model: model, registry: <any>{} })
       class User {}
     }).toThrowErrorMatchingSnapshot();
+  });
+
+  test("SequelizeObjectType can query", async () => {
+    @SequelizeObjectType({ model: model })
+    class User {
+      @Field(String)
+      fullName(this: any) {
+        return `${this.name} ${this.lastName}`;
+      }
+    }
+
+    await model.create({
+      name: "Lee",
+      lastName: "Byron"
+    });
+
+    await model.create({
+      name: "Oleg",
+      lastName: "Ilyenko"
+    });
+
+    await model.create({
+      name: "Sashko",
+      lastName: "Stubailo"
+    });
+
+    await model.create({
+      name: "Johannes",
+      lastName: "Schickling"
+    });
+
+    @ObjectType()
+    class Query {
+      @Field(User, { args: { id: ID } })
+      private getUser({ id }: { id: string }) {
+        return model.findById(id);
+      }
+
+      @Field([User])
+      private allUsers() {
+        return model.findAll();
+      }
+    }
+
+    const schema = new Schema({ query: Query });
+    var result = await schema.execute(
+      `{ allUsers { id name lastName fullName } }`
+    );
+    expect(result).toMatchSnapshot();
   });
 });
